48f8b043a0adb85d5b2449383f8420f586b340a1
celgrego <54558698+celgrego@users.noreply.github.com>
2019-09-05 16:49:06 -0400

    finishing introduction

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ utils.check_version((3,7))
 utils.clear()
 
 print('Hello, Class!')
+print('My name is Marcellous')
+print('My favorite game is gears of war')
+print('Im excited about making games in this class because i plan to make games in my future career.')
+print('I am concerned about keeping a good grade in this class and not falling behind in my work.')
+print('My stackoverflow user number is 11996584 ')
+print('the URL to my github.com profile is https://github.com/celgrego/Marcellous.git')
+
